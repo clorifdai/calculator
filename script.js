5d10d043c8e043cad67494157945ed80bde779cd
Arif <153439486+clorifdai@users.noreply.github.com>
2026-03-07 07:05:51 +0800

    Add code about number buttons

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // index.html
+
+function add(first, second) {
+    return first + second;
+}
+
+let buttons = document.querySelectorAll(".num");
+let ac = document.querySelector(".ac");
+let screen = document.querySelector(".screen-my");
+
+// Detect number button
+for (let i = 0 ; i < buttons.length; i++) {
+    let button = buttons [i];
+    button.addEventListener("click", () => {
+        if (screen.innerHTML === "0") {
+            screen.innerHTML = null;
+        }
+        screen.innerHTML += button.innerHTML});
+};
+console.log(buttons.length)
+
+ac.addEventListener("click", () => {
+    screen.innerHTML = 0
+})
